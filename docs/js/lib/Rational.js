5a690b05200d385f1af454df8f7e1b828905358b
jarett-lee <jarett.lee.pi@gmail.com>
2019-04-15 02:04:52 -0400

    Update js

--- docs/js/lib/Rational.js
+++ docs/js/lib/Rational.js
@@ -16,21 +16,73 @@ class Rational {
     denominator() {
         return this.q;
     }
+    static eq(a, b) {
+        return a.sign === b.sign && Rational.eqAbs(a, b);
+    }
+    static eqAbs(a, b) {
+        return a.p === b.p && a.q === b.q;
+    }
+    static gte(a, b) {
+        if (a.sign > b.sign)
+            return true;
+        if (a.sign < b.sign)
+            return false;
+        if (a.sign >= 0)
+            return Rational.gteAbs(a, b);
+        return !Rational.gtAbs(a, b);
+    }
+    static gt(a, b) {
+        if (a.sign > b.sign)
+            return true;
+        if (a.sign < b.sign)
+            return false;
+        if (a.sign >= 0)
+            return Rational.gtAbs(a, b);
+        return !Rational.gteAbs(a, b);
+    }
+    static gteAbs(a, b) {
+        return a.p * b.q >= b.p * a.q;
+    }
+    static gtAbs(a, b) {
+        return a.p * b.q > b.p * a.q;
+    }
+    static lte(a, b) {
+        return !Rational.gt(a, b);
+    }
+    static lt(a, b) {
+        return !Rational.gte(a, b);
+    }
+    static lteAbs(a, b) {
+        return !Rational.gtAbs(a, b);
+    }
+    static ltAbs(a, b) {
+        return !Rational.gteAbs(a, b);
+    }
     static add(a, b) {
-        throw new Error('Not implemented');
+        const numerator = BigInt(a.sign) * a.p * b.q + BigInt(b.sign) * b.p * a.q;
+        return new Rational(numerator, a.q * b.q);
     }
     static sub(a, b) {
-        throw new Error('Not implemented');
+        const numerator = BigInt(a.sign) * a.p * b.q - BigInt(b.sign) * b.p * a.q;
+        return new Rational(numerator, a.q * b.q);
     }
     static mul(a, b) {
-        throw new Error('Not implemented');
+        const sign = BigInt(a.sign * b.sign);
+        return new Rational(sign * a.p * b.p, a.q * b.q);
     }
     static div(a, b) {
-        throw new Error('Not implemented');
+        const sign = BigInt(a.sign * b.sign);
+        return new Rational(sign * a.p * b.q, a.q * b.p);
     }
     static pow(a, b) {
-        throw new Error('Not implemented');
+        if (b === 0n)
+            return new Rational(1n, 1n);
+        const r = new Rational(a.p ** b, a.q ** b);
+        r.sign = b % 2n === 0n ? 1 : a.sign;
+        return r;
     }
 }
+Rational.ZERO = new Rational(0n, 1n);
+Rational.ONE = new Rational(1n, 1n);
 export default Rational;
 //# sourceMappingURL=Rational.js.map
--- docs/js/tests/Rational.test.js
+++ docs/js/tests/Rational.test.js
@@ -22,6 +22,109 @@ describe('Rational', function () {
             chai.expect(() => new Rational(0n, 0n)).to.throw();
         });
     });
+    describe('eq', function () {
+        it('should be correct', function () {
+            chai.expect(Rational.eq(new Rational(0n, 1n), new Rational(0n, 1n))).to.be.true;
+            chai.expect(Rational.eq(new Rational(0n, 1n), new Rational(1n, 1n))).to.be.false;
+            chai.expect(Rational.eq(new Rational(0n, 1n), new Rational(-1n, 1n))).to.be.false;
+            chai.expect(Rational.eq(new Rational(1n, 1n), new Rational(1n, 1n))).to.be.true;
+            chai.expect(Rational.eq(new Rational(1n, 1n), new Rational(2n, 1n))).to.be.false;
+            chai.expect(Rational.eq(new Rational(1n, 5n), new Rational(1n, 5n))).to.be.true;
+            chai.expect(Rational.eq(new Rational(1n, 5n), new Rational(2n, 5n))).to.be.false;
+            chai.expect(Rational.eq(new Rational(9n, 5n), new Rational(9n, 5n))).to.be.true;
+            chai.expect(Rational.eq(new Rational(9n, 5n), new Rational(4n, 5n))).to.be.false;
+            chai.expect(Rational.eq(new Rational(-1n, 1n), new Rational(-1n, 1n))).to.be.true;
+            chai.expect(Rational.eq(new Rational(-1n, 1n), new Rational(-2n, 1n))).to.be.false;
+            chai.expect(Rational.eq(new Rational(-1n, 5n), new Rational(-1n, 5n))).to.be.true;
+            chai.expect(Rational.eq(new Rational(-1n, 5n), new Rational(-2n, 5n))).to.be.false;
+            chai.expect(Rational.eq(new Rational(-9n, 5n), new Rational(-9n, 5n))).to.be.true;
+            chai.expect(Rational.eq(new Rational(-9n, 5n), new Rational(-4n, 5n))).to.be.false;
+        });
+    });
+    describe('gte', function () {
+        it('should be correct', function () {
+            chai.expect(Rational.gte(new Rational(0n, 1n), new Rational(0n, 1n))).to.be.true;
+            chai.expect(Rational.gte(new Rational(0n, 1n), new Rational(1n, 1n))).to.be.false;
+            chai.expect(Rational.gte(new Rational(0n, 1n), new Rational(-1n, 1n))).to.be.true;
+            chai.expect(Rational.gte(new Rational(0n, 1n), new Rational(2n, 5n))).to.be.false;
+            chai.expect(Rational.gte(new Rational(0n, 1n), new Rational(-2n, 5n))).to.be.true;
+            chai.expect(Rational.gte(new Rational(1n, 2n), new Rational(5n, 6n))).to.be.false;
+            chai.expect(Rational.gte(new Rational(5n, 6n), new Rational(1n, 2n))).to.be.true;
+            chai.expect(Rational.gte(new Rational(1n, 2n), new Rational(1n, 2n))).to.be.true;
+            chai.expect(Rational.gte(new Rational(-1n, 2n), new Rational(-5n, 6n))).to.be.true;
+            chai.expect(Rational.gte(new Rational(-5n, 6n), new Rational(-1n, 2n))).to.be.false;
+            chai.expect(Rational.gte(new Rational(-1n, 2n), new Rational(-1n, 2n))).to.be.true;
+            chai.expect(Rational.gte(new Rational(1n, 2n), new Rational(-5n, 6n))).to.be.true;
+            chai.expect(Rational.gte(new Rational(5n, 6n), new Rational(-1n, 2n))).to.be.true;
+            chai.expect(Rational.gte(new Rational(1n, 2n), new Rational(-1n, 2n))).to.be.true;
+            chai.expect(Rational.gte(new Rational(-1n, 2n), new Rational(5n, 6n))).to.be.false;
+            chai.expect(Rational.gte(new Rational(-5n, 6n), new Rational(1n, 2n))).to.be.false;
+            chai.expect(Rational.gte(new Rational(-1n, 2n), new Rational(1n, 2n))).to.be.false;
+        });
+    });
+    describe('gt', function () {
+        it('should be correct', function () {
+            chai.expect(Rational.gt(new Rational(0n, 1n), new Rational(0n, 1n))).to.be.false;
+            chai.expect(Rational.gt(new Rational(0n, 1n), new Rational(1n, 1n))).to.be.false;
+            chai.expect(Rational.gt(new Rational(0n, 1n), new Rational(-1n, 1n))).to.be.true;
+            chai.expect(Rational.gt(new Rational(0n, 1n), new Rational(2n, 5n))).to.be.false;
+            chai.expect(Rational.gt(new Rational(0n, 1n), new Rational(-2n, 5n))).to.be.true;
+            chai.expect(Rational.gt(new Rational(1n, 2n), new Rational(5n, 6n))).to.be.false;
+            chai.expect(Rational.gt(new Rational(5n, 6n), new Rational(1n, 2n))).to.be.true;
+            chai.expect(Rational.gt(new Rational(1n, 2n), new Rational(1n, 2n))).to.be.false;
+            chai.expect(Rational.gt(new Rational(-1n, 2n), new Rational(-5n, 6n))).to.be.true;
+            chai.expect(Rational.gt(new Rational(-5n, 6n), new Rational(-1n, 2n))).to.be.false;
+            chai.expect(Rational.gt(new Rational(-1n, 2n), new Rational(-1n, 2n))).to.be.false;
+            chai.expect(Rational.gt(new Rational(1n, 2n), new Rational(-5n, 6n))).to.be.true;
+            chai.expect(Rational.gt(new Rational(5n, 6n), new Rational(-1n, 2n))).to.be.true;
+            chai.expect(Rational.gt(new Rational(1n, 2n), new Rational(-1n, 2n))).to.be.true;
+            chai.expect(Rational.gt(new Rational(-1n, 2n), new Rational(5n, 6n))).to.be.false;
+            chai.expect(Rational.gt(new Rational(-5n, 6n), new Rational(1n, 2n))).to.be.false;
+            chai.expect(Rational.gt(new Rational(-1n, 2n), new Rational(1n, 2n))).to.be.false;
+        });
+    });
+    describe('gteAbs', function () {
+        it('should be correct', function () {
+            chai.expect(Rational.gteAbs(new Rational(0n, 1n), new Rational(0n, 1n))).to.be.true;
+            chai.expect(Rational.gteAbs(new Rational(0n, 1n), new Rational(1n, 1n))).to.be.false;
+            chai.expect(Rational.gteAbs(new Rational(0n, 1n), new Rational(-1n, 1n))).to.be.false;
+            chai.expect(Rational.gteAbs(new Rational(0n, 1n), new Rational(2n, 5n))).to.be.false;
+            chai.expect(Rational.gteAbs(new Rational(0n, 1n), new Rational(-2n, 5n))).to.be.false;
+            chai.expect(Rational.gteAbs(new Rational(1n, 2n), new Rational(5n, 6n))).to.be.false;
+            chai.expect(Rational.gteAbs(new Rational(5n, 6n), new Rational(1n, 2n))).to.be.true;
+            chai.expect(Rational.gteAbs(new Rational(1n, 2n), new Rational(1n, 2n))).to.be.true;
+            chai.expect(Rational.gteAbs(new Rational(-1n, 2n), new Rational(-5n, 6n))).to.be.false;
+            chai.expect(Rational.gteAbs(new Rational(-5n, 6n), new Rational(-1n, 2n))).to.be.true;
+            chai.expect(Rational.gteAbs(new Rational(-1n, 2n), new Rational(-1n, 2n))).to.be.true;
+            chai.expect(Rational.gteAbs(new Rational(1n, 2n), new Rational(-5n, 6n))).to.be.false;
+            chai.expect(Rational.gteAbs(new Rational(5n, 6n), new Rational(-1n, 2n))).to.be.true;
+            chai.expect(Rational.gteAbs(new Rational(1n, 2n), new Rational(-1n, 2n))).to.be.true;
+            chai.expect(Rational.gteAbs(new Rational(-1n, 2n), new Rational(5n, 6n))).to.be.false;
+            chai.expect(Rational.gteAbs(new Rational(-5n, 6n), new Rational(1n, 2n))).to.be.true;
+            chai.expect(Rational.gteAbs(new Rational(-1n, 2n), new Rational(1n, 2n))).to.be.true;
+        });
+    });
+    describe('gtAbs', function () {
+        it('should be correct', function () {
+            chai.expect(Rational.gtAbs(new Rational(0n, 1n), new Rational(0n, 1n))).to.be.false;
+            chai.expect(Rational.gtAbs(new Rational(0n, 1n), new Rational(1n, 1n))).to.be.false;
+            chai.expect(Rational.gtAbs(new Rational(0n, 1n), new Rational(-1n, 1n))).to.be.false;
+            chai.expect(Rational.gtAbs(new Rational(0n, 1n), new Rational(2n, 5n))).to.be.false;
+            chai.expect(Rational.gtAbs(new Rational(0n, 1n), new Rational(-2n, 5n))).to.be.false;
+            chai.expect(Rational.gtAbs(new Rational(1n, 2n), new Rational(5n, 6n))).to.be.false;
+            chai.expect(Rational.gtAbs(new Rational(5n, 6n), new Rational(1n, 2n))).to.be.true;
+            chai.expect(Rational.gtAbs(new Rational(1n, 2n), new Rational(1n, 2n))).to.be.false;
+            chai.expect(Rational.gtAbs(new Rational(-1n, 2n), new Rational(-5n, 6n))).to.be.false;
+            chai.expect(Rational.gtAbs(new Rational(-5n, 6n), new Rational(-1n, 2n))).to.be.true;
+            chai.expect(Rational.gtAbs(new Rational(-1n, 2n), new Rational(-1n, 2n))).to.be.false;
+            chai.expect(Rational.gtAbs(new Rational(1n, 2n), new Rational(-5n, 6n))).to.be.false;
+            chai.expect(Rational.gtAbs(new Rational(5n, 6n), new Rational(-1n, 2n))).to.be.true;
+            chai.expect(Rational.gtAbs(new Rational(1n, 2n), new Rational(-1n, 2n))).to.be.false;
+            chai.expect(Rational.gtAbs(new Rational(-1n, 2n), new Rational(5n, 6n))).to.be.false;
+            chai.expect(Rational.gtAbs(new Rational(-5n, 6n), new Rational(1n, 2n))).to.be.true;
+            chai.expect(Rational.gtAbs(new Rational(-1n, 2n), new Rational(1n, 2n))).to.be.false;
+        });
+    });
     describe('add', function () {
         it('should return sum of positives', function () {
             const r1 = new Rational(0n, 1n);
@@ -57,9 +160,14 @@ describe('Rational', function () {
             rationalEq(Rational.sub(r2, r1), 1n, 2n);
             rationalEq(Rational.sub(r1, r2), -1n, 2n);
             rationalEq(Rational.sub(r2, r2), 0n, 1n);
-            rationalEq(Rational.sub(r2, r3), 1n, 4n);
+            rationalEq(Rational.sub(r2, r3), -1n, 4n);
             rationalEq(Rational.sub(r3, r4), 15n - 36n, 20n);
             rationalEq(Rational.sub(r4, r3), 36n - 15n, 20n);
+        });
+        it('should return difference of negatives', function () {
+            rationalEq(Rational.sub(new Rational(-1n, 2n), new Rational(-1n, 2n)), 0n, 1n);
+            rationalEq(Rational.sub(new Rational(1n, 2n), new Rational(-1n, 2n)), 1n, 1n);
+            rationalEq(Rational.sub(new Rational(-1n, 2n), new Rational(1n, 2n)), -1n, 1n);
         });
     });
     describe('mul', function () {
@@ -75,6 +183,9 @@ describe('Rational', function () {
             rationalEq(Rational.mul(r3, r2), 15n, 8n);
             rationalEq(Rational.mul(r3, r4), 3n * 9n, 4n);
         });
+        it('should return product of negatives', function () {
+            throw new Error('Test not yet written');
+        });
     });
     describe('div', function () {
         it('should return quotient of positives', function () {
@@ -89,7 +200,10 @@ describe('Rational', function () {
             rationalEq(Rational.div(r3, r4), 25n, 12n);
             rationalEq(Rational.div(r4, r3), 12n, 25n);
         });
-        it('should return quotient of arguments', function () {
+        it('should return product of negatives', function () {
+            throw new Error('Test not yet written');
+        });
+        it('should prevent division by zero', function () {
             const r1 = new Rational(0n, 1n);
             const r2 = new Rational(1n, 1n);
             chai.expect(() => Rational.div(r1, r1)).to.throw();
@@ -103,22 +217,24 @@ describe('Rational', function () {
             const r3 = new Rational(1n, 2n);
             const r4 = new Rational(15n, 4n);
             const r5 = new Rational(9n, 5n);
-            const r6 = new Rational(3n, 1n);
-            rationalEq(Rational.pow(r1, r1), 1n, 1n);
-            rationalEq(Rational.pow(r2, r1), 1n, 1n);
-            rationalEq(Rational.pow(r3, r1), 1n, 1n);
-            rationalEq(Rational.pow(r4, r1), 1n, 1n);
-            rationalEq(Rational.pow(r5, r1), 1n, 1n);
-            rationalEq(Rational.pow(r1, r2), 0n, 1n);
-            rationalEq(Rational.pow(r2, r2), 1n, 1n);
-            rationalEq(Rational.pow(r3, r2), 1n, 2n);
-            rationalEq(Rational.pow(r4, r2), 15n, 4n);
-            rationalEq(Rational.pow(r5, r2), 9n, 5n);
-            rationalEq(Rational.pow(r1, r6), 3n, 1n);
-            rationalEq(Rational.pow(r2, r6), 1n, 1n);
-            rationalEq(Rational.pow(r3, r6), 1n, 8n);
-            rationalEq(Rational.pow(r4, r6), 3375n, 64n);
-            rationalEq(Rational.pow(r5, r6), 27n, 1n);
+            rationalEq(Rational.pow(r1, 0n), 1n, 1n);
+            rationalEq(Rational.pow(r2, 0n), 1n, 1n);
+            rationalEq(Rational.pow(r3, 0n), 1n, 1n);
+            rationalEq(Rational.pow(r4, 0n), 1n, 1n);
+            rationalEq(Rational.pow(r5, 0n), 1n, 1n);
+            rationalEq(Rational.pow(r1, 1n), 0n, 1n);
+            rationalEq(Rational.pow(r2, 1n), 1n, 1n);
+            rationalEq(Rational.pow(r3, 1n), 1n, 2n);
+            rationalEq(Rational.pow(r4, 1n), 15n, 4n);
+            rationalEq(Rational.pow(r5, 1n), 9n, 5n);
+            rationalEq(Rational.pow(r1, 3n), 0n, 1n);
+            rationalEq(Rational.pow(r2, 3n), 1n, 1n);
+            rationalEq(Rational.pow(r3, 3n), 1n, 8n);
+            rationalEq(Rational.pow(r4, 3n), 3375n, 64n);
+            rationalEq(Rational.pow(r5, 3n), 9n ** 3n, 5n ** 3n);
+        });
+        it('should return product of negatives', function () {
+            throw new Error('Test not yet written');
         });
     });
 });
